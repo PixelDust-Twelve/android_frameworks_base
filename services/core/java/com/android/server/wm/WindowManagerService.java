ead79d2ea09904eac7e286bba1c4604b53a7e7ec
Bruno Martins <bgcngm@gmail.com>
2018-09-17 11:52:04 +0100

    Add support for runtime toggle of navbar
    jhenrique09 edits: Adapt to PE 12
    
    [spezi77] Slightly adapted:
    * Removed code for Button backlight brightness
    * StatusBar: Adapted to SettingsObserver
    
    This is a refactor of the following changes, so to make use of tuner API.
    
    Author: Timo Wendt <timo@tjwendt.de>
    Date:   Thu Aug 30 12:18:41 2012 +0300
    
        Runtime toggle of navbar
    
        This adds the framework support for enabling the Navigation bar
        on devices with hardware keys. It is toggled from Settings, and
        depends on device-specific support for the KeyDisabler hardware
        control
    
        Change-Id: I88fecb2ca1e8613591c327a93f53909b00239cd8
    
        wm: Nullify hardkey function assignments if enabling the navbar
    
        This caused erroneous (and sometimes duplicate) events being generated
        due to the regular key function assignments. The navbar does its
        own action management, so don't try to derive from the actions
        usually present in hard keys.
    
        Change-Id: I82866e24547f8145cac4f07820ae90aacce09281
    
        Update DEV_FORCE_SHOW_NAVBAR constant.
    
        Change-Id: Ie5b4317162c514d22276956f81007e064a3d0f32
    
        Settings: Move DEV_FORCE_SHOW_NAVBAR load to loadSecureSettings.
    
        Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Tue Jan 16 15:47:07 2018 +0100
    
        PhoneWindowManager: Make sure KeyDisabler is always called on boot
    
        * Otherwise, some KeyDisabler classes are left in a weird state
        * Because we don't keep track of whether an initial state was ever set,
          we never call KeyDisabler on boot if the setting is set to 0
        * To remedy this, keep track of whether an initial state was set
    
        Change-Id: Ib432ed3278dd8f4f4cba3ba488879b3c1cd9c8f4
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Sep 29 20:42:04 2018 +0200
    
        PhoneWindowManager: Fix issues introduced with runtime navbar
    
        * With system settings we need to pass UserHandle.USER_CURRENT
          to make sure we are getting proper value, otherwise we always
          end up getting '0'. Also we need to make sure to set valid
          mHasNavigationBar in setInitialDisplaySize().
    
        Change-Id: I3efd614e735f9a602f13263a742ce858a9d14769
    
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Tue Mar 24 22:04:47 2020 -0300
    
        DisplayPolicy: Fix watchdog when adding new display
    
        * Fixes system crashing after connected to Android Auto or started screen record
    
        That was introduced on toggle navbar commit
    
        Only register content observer if default display
    
        Change-Id: Ia43a922251803be94de8618eb442dcf132e479e9
    
    Change-Id: I4a6d3f89bc171c3921875b24c077cb78c03517ad
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6682,6 +6682,7 @@ public class WindowManagerService extends IWindowManager.Stub
         return mRoot.getDisplayContent(DEFAULT_DISPLAY);
     }
 
+    @Override
     public void onOverlayChanged() {
         synchronized (mGlobalLock) {
             mRoot.forAllDisplays(displayContent -> {
