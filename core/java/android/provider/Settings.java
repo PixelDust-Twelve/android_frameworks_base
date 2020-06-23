346554e7883441379206ba30d19d8b8327a15f27
DennySPb <dennyspb@gmail.com>
2020-06-23 14:34:56 +0300

    [SQUASH] SystemUI: Add more selectable VoWiFi icons [1/2]
    - OnePlus
    - Asus
    - Huawei
    - Motorola
    
    Change-Id: I20c777891f0bf0ed8e65e50758012bd876bd41d5
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Add three more selectable VoWiFi icons [1/2]
    
    from https://www.onlinewebfonts.com/
    
    Change-Id: Iadc2a5a5c87b1e6fb1fdcd48d4d1c3e7b87d6adb
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5434,6 +5434,12 @@ public final class Settings {
         public static final String VOWIFI_ICON = "vowifi_icon";
 
         /**
+         * VoWiFi icon style
+         * @hide
+         */
+        public static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
+
+        /**
          * Disable power menu on secure lock screens
          *
          * @hide
