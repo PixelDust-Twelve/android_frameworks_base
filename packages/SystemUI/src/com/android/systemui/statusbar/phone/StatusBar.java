c86b200ea626fff9561ca4417191ba6aec88c22f
spezi77 <spezi7713@gmx.net>
2021-01-16 20:31:43 +0100

    Ambient music ticker: show Now Playing info on lockscreen
    abc: keep just the nowplaying part from PixelDust @spezi77 code
    and some code cleanup
    
    Change-Id: I51fcf831451788b758bfe59c05d424a44f444315
    
    ---
    
    for the ambient display, abc music ticker code already allows to show
    both local player and Now Playing info under the clock.
    
    In lockscreen, instead, the local player shows its own media
    notification, but Now Playing info are missing.
    
    This commit adds Now Playing to lockscreen so we can cover all cases.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3332,6 +3332,12 @@ public class StatusBar extends SystemUI implements
 
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
         updateQsExpansionEnabled();
+
+        if (mAmbientIndicationContainer != null) {
+            ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                    .updateDozingState(mDozing);
+        }
+
         Trace.endSection();
     }
 
@@ -4585,6 +4591,10 @@ public class StatusBar extends SystemUI implements
                     updateScrimController();
                     mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
                     updateKeyguardState();
+                    if (mAmbientIndicationContainer != null) {
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer)
+                                .updateKeyguardState(mState == StatusBarState.KEYGUARD);
+                    }
                     Trace.endSection();
                 }
 
