01c7628d02d54c248eae03172674ac70c80d7c23
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-13 19:58:25 +0200

    Revert "Use exact brightnesses values for comparison."
    * Apparently, this is causing a hick-up or a glitch in bootanimation.
    
    This reverts commit 25fd67372ebc10a03ba587332687b596affdfcf2.
    
    Change-Id: I0988560d48eb3f8598ee60ab1e2cabfa1ae7c2ff
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/display/RampAnimator.java
+++ services/core/java/com/android/server/display/RampAnimator.java
@@ -20,6 +20,8 @@ import android.animation.ValueAnimator;
 import android.util.FloatProperty;
 import android.view.Choreographer;
 
+import com.android.internal.display.BrightnessSynchronizer;
+
 /**
  * A custom animator that progressively updates a property value at
  * a given variable rate until it reaches a particular target value.
@@ -171,7 +173,7 @@ class RampAnimator<T> {
             if (oldCurrentValue != mCurrentValue) {
                 setPropertyValue(mCurrentValue);
             }
-            if (mTargetValue != mCurrentValue) {
+            if (!BrightnessSynchronizer.floatEquals(mTargetValue, mCurrentValue)) {
                 postAnimationCallback();
             } else {
                 mAnimating = false;
