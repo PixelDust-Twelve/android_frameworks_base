370e292085d1076be63a9713ccc4aa7e9767fa28
Weijie Wang <weijiew@codeaurora.org>
2021-10-13 14:39:22 +0800

    SystemUI: Fix NPE in NotificationPanelViewController
    Change-Id: I1e52b15fc1e91b87fa299bd2f99240a045bbea14
    CRs-Fixed: 3043238
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3330,9 +3330,9 @@ public class NotificationPanelViewController extends PanelViewController {
     }
 
     private void updateKeyguardStatusBarForHeadsUp() {
-        boolean
-                showingKeyguardHeadsUp =
-                mKeyguardShowing && mHeadsUpAppearanceController.shouldBeVisible();
+        boolean showingKeyguardHeadsUp = mKeyguardShowing
+                && mHeadsUpAppearanceController != null
+                && mHeadsUpAppearanceController.shouldBeVisible();
         if (mShowingKeyguardHeadsUp != showingKeyguardHeadsUp) {
             mShowingKeyguardHeadsUp = showingKeyguardHeadsUp;
             if (mKeyguardShowing) {
