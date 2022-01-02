b03a2156ecceb03f8e3782f4334f7d4c57ed92bb
maxwen <max.weninger@gmail.com>
2022-01-02 23:34:05 +0100

    SystemUI: Allow forcing small keyguard clock [1/2]
    by default lockscreen clock will switch between big and
    small depending if notifications are shown or not
    
    but I think the small left aligned lock looks better
    especially on large screens so allow force using it
    
    idoybh (YAAP): Adapted, Improved weird logic that also affects other stuff outside of this function and could definitively cause unwanted behavior. Also actually toggle on runtime and initially, don't wait for a notification to show to actually take effect.
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ib65b15912e5dda1f56ae7014c56e11fc73f7933e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5606,6 +5606,13 @@ public final class Settings {
         public static final String RIGHT_VERTICAL_BACK_SWIPE_APP_ACTIVITY_ACTION = "right_vertical_back_swipe_app_activity_action";
 
         /**
+         * Whether to force small clock on keyguard
+         * @hide
+         */
+        @Readable
+        public static final String LOCKSCREEN_SMALL_CLOCK = "lockscreen_small_clock";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4067,6 +4067,11 @@ public class NotificationPanelViewController extends PanelViewController {
                 /* notifyForDescendants */ false,
                 mSettingsChangeObserver
         );
+        mContentResolver.registerContentObserver(
+                Settings.System.getUriFor(Settings.System.LOCKSCREEN_SMALL_CLOCK),
+                /* notifyForDescendants */ false,
+                mSettingsChangeObserver
+        );
     }
 
     private void unregisterSettingsChangeListener() {
@@ -4435,6 +4440,8 @@ public class NotificationPanelViewController extends PanelViewController {
 
             // Can affect multi-user switcher visibility
             reInflateViews();
+            // To update forcing small clock on keyguard
+            positionClockAndNotifications();
         }
     }
 
