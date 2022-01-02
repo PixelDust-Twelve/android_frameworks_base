989f1d011cc6d146ab7e49c24f3277ee632e2822
maxwen <max.weninger@gmail.com>
2022-01-02 23:34:05 +0100

    SystemUI: Allow forcing small keyguard clock [1/2]
    by default lockscreen clock will switch between big and
    small depending if notifications are shown or not
    
    but I think the small left aligned lock looks better
    especially on large screens so allow force using it
    
    idoybh (YAAP): Adapted, Improved weird logic that also affects other stuff outside of this function and could definitively cause unwanted behavior. Also actually toggle on runtime and initially, don't wait for a notification to show to actually take effect.
    
    Change-Id: I10646527ebff6d331d5b68e0adb5022a6e0d1324
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4096,6 +4096,11 @@ public class NotificationPanelViewController extends PanelViewController {
                 /* notifyForDescendants */ false,
                 mSettingsChangeObserver
         );
+        mContentResolver.registerContentObserver(
+                Settings.System.getUriFor(Settings.System.LOCKSCREEN_SMALL_CLOCK),
+                /* notifyForDescendants */ false,
+                mSettingsChangeObserver
+        );
     }
 
     private void unregisterSettingsChangeListener() {
@@ -4465,6 +4470,8 @@ public class NotificationPanelViewController extends PanelViewController {
 
             // Can affect multi-user switcher visibility
             reInflateViews();
+            // To update forcing small clock on keyguard
+            positionClockAndNotifications();
         }
     }
 
