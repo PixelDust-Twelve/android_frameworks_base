d8a8a37ec671cd76c03be581023909b6e1b6882a
kdrag0n <danny@kdrag0n.dev>
2022-01-28 17:54:02 +0100

    Gestures nav: Add Power menu action
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I11cbb003a03b17a981d957cf25aa28138426802d

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1037,6 +1037,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 15: // Previous song
                 PixeldustUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
+            case 16: // Powermenu
+                PixeldustUtils.showPowerMenu();
+                break;
         }
     }
 
