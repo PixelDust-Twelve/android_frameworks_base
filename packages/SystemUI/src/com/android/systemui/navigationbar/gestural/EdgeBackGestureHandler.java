011b97b51ef99526bb5b5f1ca39602d28b531c84
spezi77 <spezi7713@gmx.net>
2022-01-28 17:56:51 +0100

    Gestures nav: Add Partial Screenshot action. * Switch to the method in our utils, as this also supports the partial screenshot
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1014,7 +1014,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                PixeldustUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1039,6 +1039,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 PixeldustUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                PixeldustUtils.takeScreenshot(false);
                 break;
         }
     }
