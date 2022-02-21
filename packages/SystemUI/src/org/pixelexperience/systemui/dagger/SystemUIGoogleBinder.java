Evolution-X-AOSP/frameworks_base

SystemUI: Use Custom ThemeoverlayController instead of Google ThemeOverlayController
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
@@ -43,7 +43,7 @@ import com.android.systemui.toast.ToastUI;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.wmshell.WMShell;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import org.protonaosp.systemui.theme.CustomThemeOverlayController;
 import com.android.systemui.dagger.SysUISingleton;
 
 import org.pixelexperience.systemui.GoogleServices;
@@ -168,7 +168,7 @@ public abstract class SystemUIGoogleBinder {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController.class)
-    public abstract SystemUI bindThemeOverlayController(ThemeOverlayControllerGoogle sysui);
+    public abstract SystemUI bindThemeOverlayController(CustomThemeOverlayController sysui);
 
     /**
      * Inject into ToastUI.

PixelDust-Twelve/android_frameworks_base

Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: I44358b21ed4f1302a172c9d79edff0b1c42a9b45

packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleBinder.java
@@ -44,7 +44,7 @@ import com.android.systemui.toast.ToastUI;
 import com.android.systemui.util.leak.GarbageMonitor;
 import com.android.systemui.volume.VolumeUI;
 import com.android.systemui.wmshell.WMShell;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+import com.pixeldust.systemui.theme.CustomThemeOverlayController;
 import com.android.systemui.dagger.SysUISingleton;
 
 import org.pixelexperience.systemui.GoogleServices;
@@ -170,7 +170,7 @@ public abstract class SystemUIGoogleBinder {
     @Binds
     @IntoMap
     @ClassKey(ThemeOverlayController.class)
-    public abstract SystemUI bindThemeOverlayController(ThemeOverlayControllerGoogle sysui);
+    public abstract SystemUI bindThemeOverlayController(CustomThemeOverlayController sysui);
 
     /**
      * Inject into ToastUI.