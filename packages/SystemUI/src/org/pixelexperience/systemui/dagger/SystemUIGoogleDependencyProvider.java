7b0927ca3d5bff3fa8cfb23f228fdf6c624588b1
shivatejapeddi <shivatejapeddi@gmail.com>
2022-03-31 18:54:08 +0200

    SystemUI: Use our ThemeOverlayController instead of Google's.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleDependencyProvider.java
+++ packages/SystemUI/src/org/pixelexperience/systemui/dagger/SystemUIGoogleDependencyProvider.java
@@ -240,7 +240,8 @@ import com.google.android.systemui.statusbar.notification.voicereplies.Notificat
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyLogger;
 import com.google.android.systemui.statusbar.notification.voicereplies.NotificationVoiceReplyManager;
 import com.google.android.systemui.statusbar.phone.WallpaperNotifier;
-import com.google.android.systemui.theme.ThemeOverlayControllerGoogle;
+
+import com.pixeldust.android.systemui.theme.ThemeOverlayControllerPixeldust;
 
 import org.pixelexperience.systemui.GoogleServices;
 import org.pixelexperience.systemui.assist.AssistManagerGoogle;
@@ -418,8 +419,8 @@ public class SystemUIGoogleDependencyProvider {
 
     @Provides
     @SysUISingleton
-    static ThemeOverlayControllerGoogle provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, SystemPropertiesHelper systemPropertiesHelper, @Main Resources resources, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle, ConfigurationController configurationController) {
-        return new ThemeOverlayControllerGoogle(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, systemPropertiesHelper, resources, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle, configurationController);
+    static ThemeOverlayControllerPixeldust provideThemeOverlayControllerGoogle(Context context, BroadcastDispatcher broadcastDispatcher, @Background Handler handler, @Main Executor executor, @Background Executor executorB, ThemeOverlayApplier themeOverlayApplier, SecureSettings secureSettings, SystemPropertiesHelper systemPropertiesHelper, @Main Resources resources, WallpaperManager wallpaperManager, UserManager userManager, DumpManager dumpManager, DeviceProvisionedController deviceProvisionedController, UserTracker userTracker, FeatureFlags featureFlags, WakefulnessLifecycle wakefulnessLifecycle, ConfigurationController configurationController) {
+        return new ThemeOverlayControllerPixeldust(context, broadcastDispatcher, handler, executor, executorB, themeOverlayApplier, secureSettings, systemPropertiesHelper, resources, wallpaperManager, userManager, dumpManager, deviceProvisionedController, userTracker, featureFlags, wakefulnessLifecycle, configurationController);
     }
 
     @Provides
